1956d44f808df4c68ad6df0504d117c790342924
Ireaurora <ireaurora@gmail.com>
2020-07-24 15:59:03 +0100

    Works

--- LocalStorageEventDelegation/local.js
+++ LocalStorageEventDelegation/local.js
@@ -1,4 +1,3 @@
-
 window.onload = function() {
   const addItems = document.querySelector('.add-items');
   const itemsList = document.querySelector('.plates');
@@ -16,11 +15,10 @@ function addItem(e){
       //text is whatever the user has put 
       text,
       done: false
-    }
+    };
 
     items.push(item);
     populateList(items, itemsList);
-
     //you can only use strings in local storage
     //local storage is a key-value store  
     //Localstorage converts everything to a string by using toString() therefore you need to stringify it
@@ -31,25 +29,27 @@ function addItem(e){
 }
 //you can pass in any array as input and any array as output and it'll still work
 //everytime you add an item you recreate the list -- you might want to consider a better solutions -- react or angular
-function populateList(plates = [], platesList){
-  platesList.innerHTML = plates.map((plate, i ) => {
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
     return `
-    <li>
-    <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked': ''}/>
-    <label for="">${plate.text}</label>
-    </li>
-    `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+        <label for="item${i}">${plate.text}</label>
+      </li>
+    `;
   }).join('');
 }
-populateList(items, itemsList);
 
-
-function toggleDone(e){
-    if(!e.target.matches('input')) return; //skip this unless it's an input
-  const el = e.target; 
+function toggleDone(e) {
+  if (!e.target.matches('input')) return; // skip this unless it's an input
+  const el = e.target;
   const index = el.dataset.index;
   items[index].done = !items[index].done;
   localStorage.setItem('items', JSON.stringify(items));
-  populateList(items, itemsList);  
-}
+  populateList(items, itemsList);
+  }
+
+  addItems.addEventListener('submit', addItem);
+  itemsList.addEventListener('click', toggleDone);
+  populateList(items, itemsList);
 }
